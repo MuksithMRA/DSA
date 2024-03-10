cb5e8cca714ed967a7c72c642d1e64789cbb532f
MuksithMRA <muksithmra@gmail.com>
2024-03-10 15:41:18 +0530

    REF

--- Main.java
+++ Main.java
@@ -140,22 +140,140 @@ class LinkedList<T> {
 
 }
 
+class DoublyLinkedList<T> {
+    Node<T> first;
+    Node<T> last;
+
+    private class Node<T> {
+        private T value;
+        private Node<T> next;
+        private Node<T> prev;
+
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public void addLast(T value) {
+        Node<T> node = new Node<>(value);
+
+        if (first == null) {
+            first = last = node;
+        } else {
+            node.prev = last;
+            last.next = node;
+            last = node;
+        }
+    }
+
+    public void addFirst(T value) {
+        Node<T> node = new Node<>(value);
+
+        if (first == null) {
+            first = last = node;
+        } else {
+            node.next = first;
+            first.prev = node;
+            first = node;
+        }
+    }
+
+    public void insertBefore(T key, T value) {
+        Node<T> node = new Node<>(value);
+
+        if (first == null)
+            first = last = node;
+        else {
+            Node<T> currNode = first;
+            while (currNode != null && currNode.value != key)
+                currNode = currNode.next;
+            if (currNode == null || currNode.value == first.value) {
+                addFirst(value);
+                return;
+            }
+            node.next = currNode;
+            node.prev = currNode.prev;
+            if (currNode.prev != null) {
+                currNode.prev.next = node;
+            }
+            currNode.prev = node;
+        }
+    }
+
+    public void insertAfter(T key, T value) {
+        Node<T> node = new Node<>(value);
+
+        if (first == null)
+            first = last = node;
+        else {
+            Node<T> currNode = first;
+
+            while (currNode != null && currNode.value != key)
+                currNode = currNode.next;
+            if(currNode == null || currNode.value == last.value){
+                addLast(value);
+                return;
+            }
+            node.prev = currNode;
+            node.next = currNode.next;
+            if(currNode.next != null){
+                currNode.next.prev = node;
+            }
+            currNode.next = node;
+
+        }
+    }
+
+    public void display() {
+        Node<T> currNode = first;
+        System.out.println("Forward");
+        System.out.print("[");
+        while (currNode != null) {
+            System.out.print(currNode.value + ", ");
+            currNode = currNode.next;
+        }
+        System.out.println("]");
+
+        System.out.println("Backward");
+        currNode = last;
+        System.out.print("[");
+        while (currNode != null) {
+            System.out.print(currNode.value + ", ");
+            currNode = currNode.prev;
+        }
+        System.out.println("]");
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
+        // LinkedList<Integer> list = new LinkedList<>();
+        // list.addLast(10);
+        // list.addLast(20);
+        // list.addLast(30);
+        // list.addFirst(50);
+        // list.insertAfter(20, 100);
+        // list.insertBefore(100, 150);
+        // System.out.println(list.toArray().toString());
+        // System.out.println(list.size());
+        // System.out.println(list.indexOf(20));
+        // System.out.println(list.contains(20));
+        // list.removeFirst();
+        // System.out.println(list.size());
+        // list.removeLast();
+        // System.out.println(list.size());
+
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        list.addLast(5);
         list.addLast(10);
         list.addLast(20);
         list.addLast(30);
-        list.addFirst(50);
-        list.insertAfter(20, 100);
-        list.insertBefore(100, 150);
-        System.out.println(list.toArray().toString());
-        System.out.println(list.size());
-        System.out.println(list.indexOf(20));
-        System.out.println(list.contains(20));
-        list.removeFirst();
-        System.out.println(list.size());
-        list.removeLast();
-        System.out.println(list.size());
+        list.addFirst(40);
+        list.insertBefore(30, 150);
+        list.insertAfter(40, 200);
+        list.display();
     }
 }
